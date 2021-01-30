1990c4716187ccb7238f6bff0521180d82d817bc
Quratulain2 <49489075+Quratulain2@users.noreply.github.com>
2021-01-30 18:31:15 +0500

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,124 @@
+import React, { useState } from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import { SidebarData } from './SidebarData';
+import './Navbar.css';
+import { IconContext } from 'react-icons';
+import { Button } from "./Button"
+
+
+function Navbar() {
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: '#fff' }}>
+        <div className='navbar'>
+          
+          <Link to='#' className='menu-bars'>
+            <FaIcons.FaBars onClick={showSidebar} />
+          </Link>
+          <h1>NEDUET</h1>
+          {/* <Link to="/signup" ><Button >Sign In</Button> </Link> */}
+        </div>
+        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+          <ul className='nav-menu-items' onClick={showSidebar}>
+            <li className='navbar-toggle'>
+              <Link to='#' className='menu-bars'>
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+         
+        </nav>
+      </IconContext.Provider>
+    </>
+  );
+}
+
+export default Navbar;
+// import React, { useState } from 'react';
+// import styled from 'styled-components';
+// import { Link } from 'react-router-dom';
+// import * as FaIcons from 'react-icons/fa';
+// import * as AiIcons from 'react-icons/ai';
+// import { SidebarData } from './SidebarData';
+// import SubMenu from './SubMenu';
+// import { IconContext } from 'react-icons/lib';
+
+// const Nav = styled.div`
+//   background: #15171c;
+//   height: 80px;
+//   display: flex;
+//   justify-content: flex-start;
+//   align-items: center;
+// `;
+
+// const NavIcon = styled(Link)`
+//   margin-left: 2rem;
+//   font-size: 2rem;
+//   height: 80px;
+//   display: flex;
+//   justify-content: flex-start;
+//   align-items: center;
+// `;
+
+// const SidebarNav = styled.nav`
+//   background: #15171c;
+//   width: 250px;
+//   height: 100vh;
+//   display: flex;
+//   justify-content: center;
+//   position: fixed;
+//   top: 0;
+//   left: ${({ sidebar }) => (sidebar ? '0' : '-100%')};
+//   transition: 350ms;
+//   z-index: 10;
+// `;
+
+// const SidebarWrap = styled.div`
+//   width: 100%;
+// `;
+
+// const Sidebar = () => {
+//   const [sidebar, setSidebar] = useState(false);
+
+//   const showSidebar = () => setSidebar(!sidebar);
+
+//   return (
+//     <>
+//       <IconContext.Provider value={{ color: '#fff' }}>
+//         <Nav>
+//           <NavIcon to='#'>
+//             <FaIcons.FaBars onClick={showSidebar} />
+//           </NavIcon>
+//         </Nav>
+//         <SidebarNav sidebar={sidebar}>
+//           <SidebarWrap>
+//             <NavIcon to='#'>
+//               <AiIcons.AiOutlineClose onClick={showSidebar} />
+//             </NavIcon>
+//             {SidebarData.map((item, index) => {
+//               return <SubMenu item={item} key={index} />;
+//             })}
+//           </SidebarWrap>
+//         </SidebarNav>
+//       </IconContext.Provider>
+//     </>
+//   );
+// };
+
+// export default Sidebar;
